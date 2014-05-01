d2b0743e9b906bb664e315835b1e54f7ad217a30
Alex Navasardyan <twokul@users.noreply.github.com>
2014-05-01 12:31:47 -0400

    failing tests

--- test/yam-test.js
+++ test/yam-test.js
@@ -16,4 +16,40 @@ describe('Yam tests', function() {
     assert.ok(yam.clear);
     assert.ok(yam.remove);
   });
+
+  it('should return the value of the specified key', function() {
+    assert.ok(false);
+  });
+
+  it('should return `null` as a value for non-existing key', function() {
+    assert.ok(false);
+  });
+
+  it('should create and set the value for an non-existing key', function() {
+    assert.ok(false);
+  });
+
+  it('should set the value for an existing key', function() {
+    assert.ok(false);
+  });
+
+  it('should clear all the values upon `clear` call', function() {
+    assert.ok(false);
+  });
+
+  it('shouldn\'t throw exception upon deletion of an non-existing key', function() {
+    assert.ok(false);
+  });
+
+  it('should delete the specified key upon `delete` call', function() {
+    assert.ok(false);
+  });
+
+  it('should use an existing config file if it exist', function() {
+    assert.ok(false);
+  });
+
+  it('should create a config file if it doesn\'t exist', function() {
+    assert.ok(false);
+  });
 });
