71239cb691bb8335b00d0332a12ffa8df981bb9a
Alex Navasardyan <twokul@users.noreply.github.com>
2014-05-08 16:39:12 -0400

    green tests

--- test/yam-test.js
+++ test/yam-test.js
@@ -6,6 +6,8 @@ var assert = require('chai').assert,
     yam;
 
 describe('Yam', function() {
+  var path = '.test';
+
   it('exists', function() {
     assert.ok(Yam);
   });
@@ -18,9 +20,6 @@ describe('Yam', function() {
     assert.ok(yam.clear);
     assert.ok(yam.remove);
     assert.ok(yam.storageExists);
-    assert.ok(yam.forceSet);
-    assert.ok(yam.forceClear);
-    assert.ok(yam.forceRemove);
   });
 
   it('private API methods exist', function() {
@@ -30,144 +29,133 @@ describe('Yam', function() {
     assert.ok(yam._internalOptions);
   });
 
-  describe('storageExists', function() {
-    var path = '.settings';
+  describe('with `force` option', function() {
+    describe('.storageExists()', function() {
+      beforeEach(function() {
+        yam = new Yam('test', {
+          force: true,
+          options: {
+            foo: 'bar'
+          }
+        });
+      });
 
-    beforeEach(function() {
-      yam = new Yam('test', path, {
-        foo: 'bar'
+      it('should return true if storage file exists', function() {
+        assert.ok(yam.storageExists());
+        fs.unlinkSync(path);
+      });
+
+      it('should return false if storage file doesn\'t exists', function() {
+        fs.unlinkSync(path);
+        assert.notOk(yam.storageExists());
       });
     });
 
-    it('should return true if storage file exists', function() {
-      assert.ok(yam.storageExists());
-      fs.unlinkSync(path);
-    });
+    describe('.get()', function() {
+      beforeEach(function() {
+        yam = new Yam('test', {
+          force: true,
+          options: {
+            foo: 'bar'
+          }
+        });
+      });
 
-    it('should return false if storage file doesn\'t exists', function() {
-      fs.unlinkSync(path);
-      assert.notOk(yam.storageExists());
-    });
-  });
-
-  describe('get', function() {
-    var path = '.settings';
-
-    afterEach(function() {
-      fs.unlinkSync(path);
-    });
-
-    it('should return the value of the specified key', function() {
-      yam = new Yam('test', path, { foo: 'bar' });
-      assert.equal(yam.get('foo'), 'bar');
-    });
-
-    it('should return `null` as a value for non-existing key', function() {
-      yam = new Yam('test', path);
-      yam.get('foo', 'bar');
-      assert.equal(yam.get('foo'), null);
-    });
-  });
-
-  describe('set', function() {
-    var path = '.settings';
-
-    afterEach(function() {
-      fs.unlinkSync(path);
-    });
-
-    it('should create and set the value for an non-existing key', function() {
-      yam = new Yam('test', path);
-
-      yam.set('foo', 'bar');
-      assert.equal(yam.get('foo'), 'bar');
-    });
-
-    it('should set the value for an existing key', function() {
-      yam = new Yam('test', path, { foo: 'bar' });
-
-      assert.equal(yam.get('foo'), 'bar');
-      yam.set('foo', 'baz');
-      assert.equal(yam.get('foo'), 'baz');
-    });
-  });
-
-  describe('clear', function() {
-    var path = '.settings';
-
-    afterEach(function() {
-      fs.unlinkSync(path);
-    });
-
-    it('should clear all the values upon `clear` call', function() {
-      yam = new Yam('test', path, { foo: 'bar' });
-
-      assert.equal(yam.get('foo'), 'bar');
-      yam.clear();
-      assert.equal(yam.get('foo'), null);
-
-      assert.equal(fs.readFileSync(path), '{}');
-    });
-  });
-
-  describe('remove', function() {
-    var path = '.settings';
-
-    afterEach(function() {
-      fs.unlinkSync(path);
-    });
-
-    it('shouldn\'t throw exception upon deletion of an non-existing key', function() {
-      yam = new Yam('test', path);
-
-      assert.throws(function() {
-        yam.remove('foo');
-      }, '`foo` doesn\'t exist.');
-    });
-
-    it('should delete the specified key upon `remove` call', function() {
-      yam = new Yam('test', path, { foo: 'bar' });
-      yam.remove('foo');
-      assert.equal(yam.get('foo'), null);
-    });
-  });
-
-  describe('constructor', function() {
-    var path = '.test';
-
-    afterEach(function() {
-      if (fs.existsSync(path)) {
+      afterEach(function() {
         fs.unlinkSync(path);
-      }
+      });
+
+      it('should return the value of the specified key', function() {
+        yam = new Yam('test', {
+          force: true,
+          options: {
+            foo: 'bar'
+          }
+        });
+        assert.equal(yam.get('foo'), 'bar');
+      });
+
+      it('should return `null` as a value for non-existing key', function() {
+        yam = new Yam('test', path);
+        yam.get('foo', 'bar');
+        assert.equal(yam.get('foo'), null);
+      });
     });
 
-    it('should use an existing config file if it exist', function() {
-      fs.openSync(path, 'w');
-      yam = new Yam('test', path);
-      assert.equal(yam.path, path);
+    describe('.set()', function() {
+      afterEach(function() {
+        fs.unlinkSync(path);
+      });
+
+      it('should create and set the value for an non-existing key', function() {
+        yam = new Yam('test', {
+          force: true
+        });
+
+        yam.set('foo', 'bar');
+        assert.equal(yam.get('foo'), 'bar');
+      });
+
+      it('should set the value for an existing key', function() {
+        yam = new Yam('test', {
+          force: true,
+          options: {
+            foo: 'bar'
+          }
+        });
+
+        assert.equal(yam.get('foo'), 'bar');
+        yam.set('foo', 'baz');
+        assert.equal(yam.get('foo'), 'baz');
+      });
     });
 
-    it('should throw an exception if `id` is not specified', function() {
-      assert.throws(function() {
-        yam = new Yam();
-      }, 'You must specify an id.');
+    describe('.clear()', function() {
+      afterEach(function() {
+        fs.unlinkSync(path);
+      });
+
+      it('should clear all the values upon `clear` call', function() {
+        yam = new Yam('test', {
+          force: true,
+          options: {
+            foo: 'bar'
+          }
+        });
+
+        assert.equal(yam.get('foo'), 'bar');
+        yam.clear();
+        assert.equal(yam.get('foo'), null);
+
+        assert.equal(fs.readFileSync(path), '{}');
+      });
     });
 
-    it('should create a default storage file if `path` wasn\'t specified', function() {
-      yam = new Yam('test');
-      assert.ok(fs.existsSync(path));
-    });
+    describe('remove', function() {
+      afterEach(function() {
+        fs.unlinkSync(path);
+      });
 
-    it('should create a config file and populate it with `options`', function() {
-      yam = new Yam('test', path, { foo: 'bar' });
+      it('shouldn\'t throw exception upon deletion of an non-existing key', function() {
+        yam = new Yam('test', {
+          force: true
+        });
 
-      assert.ok(fs.existsSync(path));
-      assert.equal(yam.get('foo'), 'bar');
-    });
+        assert.throws(function() {
+          yam.remove('foo');
+        }, '`foo` doesn\'t exist.');
+      });
 
-    it('should create a config file if it doesn\'t exist', function() {
-      yam = new Yam('test', path, {});
-      assert.ok(fs.existsSync(path));
+      it('should delete the specified key upon `remove` call', function() {
+        yam = new Yam('test', {
+          force: true,
+          options: {
+            foo: 'bar'
+          }
+        });
+        yam.remove('foo');
+        assert.equal(yam.get('foo'), null);
+      });
     });
   });
 });
