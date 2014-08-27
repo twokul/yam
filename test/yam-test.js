de7fbca389e52e4b955580ad5d15f3c0be8a743f
Alex Navasardyan <twokul@users.noreply.github.com>
2014-08-27 18:56:27 -0400

    adds support for `get`

--- test/yam-test.js
+++ test/yam-test.js
@@ -6,10 +6,32 @@ var yam;
 
 var ok        = assert.ok;
 var deepEqual = assert.deepEqual;
+var equal     = assert.equal;
 
 describe('Yam', function() {
   it('exists', function() {
     ok(Yam);
+  });
+
+  describe('get', function() {
+    it('returns value', function() {
+      yam = new Yam('test', {
+        primary:   'test/fixtures/primary/',
+        secondary: 'test/fixtures/secondary/'
+      });
+
+      equal(yam.get('foo'), 'bar');
+      equal(yam.get('baz'), 5);
+    });
+
+    it('returns `undefined` if the value was not found', function() {
+      yam = new Yam('test', {
+        primary:   'test/fixtures/primary/',
+        secondary: 'test/fixtures/secondary/'
+      });
+
+      equal(yam.get('durp'), undefined);
+    });
   });
 
   describe('constructor', function() {
